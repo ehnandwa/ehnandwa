2802a25bd35dab6469b1b91790d000398ea8d7bb
Nandwa <nandwaharry24@gmail.com>
2021-09-29 19:19:09 +0300

    error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,22 @@
+import { NextSeo } from "next-seo"
+
+function ErrorPage() {
+  const SEO = {
+    title: "unexpected error",
+    description: "unexpected error",
+    openGraph: {
+      title: "unexpected error",
+      description: "unexpected error",
+    },
+  }
+  return (
+    <>
+      <NextSeo {...SEO} />
+
+      <div className="lg:pt-96 pt-24 font-bold text-center lg:text-3xl">
+        OOps Unexpected error Something Went Wrong
+      </div>
+    </>
+  )
+}
+export default ErrorPage
