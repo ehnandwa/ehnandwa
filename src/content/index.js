60220b66fc6c7274bc80705decfc5a52d6fb0ea8
nandwa <nandwaharry24@gmail.com>
2022-01-01 12:41:43 +0300

    u

--- src/content/index.js
+++ src/content/index.js
@@ -2,7 +2,7 @@ export default {
   header: {
     img: "https://assets.pesaledger.com/cms/378bb8497a8410e7372053d65908747e.jpg",
     text: ["Hi", "It's me .", "I am a"],
-    typical: ["web developer", 5000, "Fintech enthusiast", 5000],
+    typical: ["web developer", 5000],
     btnText: "Discover More",
   },
 };
