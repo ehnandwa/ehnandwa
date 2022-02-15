1a5c6266bfc3a52a7d769f3f4a277dc319dff4a8
nandwa <nandwaharry24@gmail.com>
2022-02-15 22:46:39 +0300

    remove image

--- src/components/Cards/CardBackground/CardWithBackground.tsx
+++ src/components/Cards/CardBackground/CardWithBackground.tsx
@@ -1,27 +1,16 @@
 import Link from "next/link";
-import Image from "next/image";
 
 function CardWithBackground() {
   return (
     <div className="pt-16 flex flex-col lg:flex-row items-center">
       <div className="">
-        <div className="">
-          <div className="">
-            <Image
-              width={48}
-              height={48}
-              src="https://assets.pesaledger.com/cms/378bb8497a8410e7372053d65908747e.jpg"
-              alt=""
-              className="rounded-full"
-            />
-          </div>
 
           <Link href="/">
             <a className="block text-yellow-400 font-bold tracking-wide flex">
               <span>Lets Connect</span>
             </a>
           </Link>
-        </div>
+
       </div>
     </div>
   );
